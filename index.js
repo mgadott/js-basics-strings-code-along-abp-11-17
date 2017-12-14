6b4c763f8f179e94fcc33ea8bca0f46d563730ef
Owolabi Mukaila <mgadott@gmail.com>
2017-12-14 20:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-//const currentUser = 'Edsger Dijkstra';
 
 ...
 
 const shortGreeting = 'Welcome, ' ;
+
+const currentUser = 'Grace Hopper';
+
+...
+
+const shortGreeting = `Welcome, ${currentUser}`;
